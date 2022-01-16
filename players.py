11a9046f6efcb723e8592478369717cba8230dea
koaladoesit <o@ok.nz>
2022-01-16 22:03:16 +0000

    Bug solved: blank card at -2 stays in player's initial hand. Added comments.

--- players.py
+++ players.py
@@ -1,17 +1,22 @@
 from shoe import shoe
-#create a Player class so we can play around with wins, losses and bets,
-#also store hands
+'''
+- There is a maximum of 3 players in a game, and one dealer
+- Each player is dealt a two - cards hand at the beginning of the game, face-up.
+- The dealer gets 2 cards as well, but one of them is face-down(hidden)
+'''
 
 class Player:
 
     name = ""
-    busted = False
-    tie = False
+    busted = False # A player is busted if they go over 21.
+                    #Need to record this so we know when all players are done with the current game
+    tie = False #player ties if they have the same score as the dealer
+                #(except for a blackjack on either side), or a blackjack when dealer has one
     lost = False
     win = False
-    stay = False
-    wins = 0
-    losses = 0
+    stay = False #Need to record this so we know when all players are done with the current game
+    wins = 0 #For future development
+    losses = 0 # idem
     has_blank = False
 
 
@@ -19,33 +24,27 @@ class Player:
 #to fill in as many of the three places as requested by user)
     def __init__(self, number):
         if number != 4 and self.name == "":
-            self.name = input("Player {}, please enter your name: ".format(number)) # in game_play we use a list iteration, so it starts at zero
+            self.name = input("Player {}, please enter your name: ".format(number))
             self.bet = []
         elif number == 4:
             self.name = "Dealer"
         self.hand = []
 
 # first hand is of two cards for each player (including the dealer) -
-# needs help with the mutable element as argument  gotcha
+# included in init to solve the mutable element as argument gotcha (yes, I've hit it)
         def deal_first_hand():
-            if shoe[-1][0] == "BLANK" or shoe[-2][0]=="BLANK":
+            if shoe[-1][0] == "BLANK":
                 self.has_blank = True
                 shoe.pop() #remove blank card so it doesn't get played
+            if shoe[-2][0]=="BLANK":
+                self.has_blank = True
+                shoe.pop(-2) #remove blank card so it doesn't get played
             self.hand.append(shoe.pop())
             self.hand.append(shoe.pop())
         deal_first_hand()
-#player needs to see the cards
 #to be used when player asks to hit:
     def deal(self):
         if shoe[-1][0] == "BLANK":
             self.has_blank = True
             shoe.pop()# remove blank card so it doesn't get played
         self.hand.append(shoe.pop())
-
-#let's see if we can factor in some gambling!
-#How do I make sure that the bet is an int?
-    def place_bet(self):
-        min_bet = 2
-        max_bet = 10
-        print('Minimum bet is 2, maximum is 10')
-        self.bet = input("{}, please place your bet! Enter the number of chips: ".format(self.name))
